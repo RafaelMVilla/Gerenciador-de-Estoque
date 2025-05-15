7f8e1d926d26fe978fba7e2e44879aa0848975df
Igor Hey Matos <iheymatos1@gmail.com>
2025-05-14 21:36:26 -0300

    FRONT - Criação do POST

--- StockUp/front/script.js
+++ StockUp/front/script.js
@@ -1,7 +1,19 @@
+// Constantes responsáveis para listar todos os itens
 const btnItens = document.getElementById('btn-itens');
 const listaItens = document.getElementById('item-list');
+
+// Constantes responsáveis para criar
+const formItem = document.getElementById('form-item-add');
+const inputDispositivo = document.getElementById('input-item-dispositivo');
+const inputMarca = document.getElementById('input-item-marca');
+const inputModelo = document.getElementById('input-item-modelo');
+const inputResponsavel = document.getElementById('input-item-responsavel');
+const inputLocal = document.getElementById('input-item-local');
+
+// Constante responsável pela URL da API
 const stockUpURL = "http://localhost:5013/api/Item";
 
+// Constante responsável para pegar todos os itens
 const getItens = async () => {
     listaItens.innerHTML = "";
 
@@ -32,7 +44,49 @@ const getItens = async () => {
     }
 }
 
+// Constante responsável por criar um novo item
+const postItem = async (novoItem) => {
+    listaItens.innerHTML = '';
+
+    try {
+        const response = await fetch(stockUpURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(novoItem)
+        });
+
+        
+        if(!response.ok){
+            throw new Error("Erro ao criar novo item!");
+        }
+    
+        const item = await response.json();
+
+        alert(`O item ${item.dispositivo} foi adicionado com sucesso!`);
+
+    } catch (error) {
+        console.log(error.message);
+        alert(error.message);
+    }
+}
+
+
+// Função para listar todos os itens pelo botão "btn-itens"
 btnItens.addEventListener('click', (e) => {
     e.preventDefault();
     getItens();
+});
+
+// Função para criar um novo item pelo form
+formItem.addEventListener('submit', (e) => {
+    e.preventDefault();
+    postItem({
+        dispositivo: inputDispositivo.value,
+        marca: inputMarca.value,
+        modelo: inputModelo.value,
+        responsavel: inputResponsavel.value,
+        local: inputLocal.value,
+    });
 });
