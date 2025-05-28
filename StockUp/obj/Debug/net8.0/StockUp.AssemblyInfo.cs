c546b91acfc7716584081feea0bed4ff080c7eb3
Igor Hey Matos <iheymatos1@gmail.com>
2025-05-28 20:02:10 -0300

    Salvando alterações local

--- StockUp/obj/Debug/net8.0/StockUp.AssemblyInfo.cs
+++ StockUp/obj/Debug/net8.0/StockUp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358ea45ce51d6f5eba0181877db7ff6a72b6e961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee2170ab639471261ec1e05eb35de4bb6b132a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
